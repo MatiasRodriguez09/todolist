ddc1893346550ccd9ad3ab372e9a10538906b6f6
MatiasRodriguez09 <rodriguezlezcanomatias@gmail.com>
2023-01-13 18:37:57 +0000

    My first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,64 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+import React, {useState} from "react";
+// create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
 
+
+    const [tarea, setTarea] = useState("");
+    const [lista, setLista] = useState([]);
+
+
+    const DeleteItems = (indexItem) => {
+      setLista((prevState) =>
+        prevState.filter((listItems, index) => index !== indexItem)
+      );
+      };
+
+    function enviarDatos(e) {
+        e.preventDefault();
+        setLista([
+            ...lista,
+            tarea])
+            setTarea("");
+    }
+    console.log(lista);
+    return (
+        <>
+            <div className="container">
+               
+                <div className="card">
+                    <div className="card-body">
+                        <input type="text" className="input m-1 w-75" value={tarea} id="exampleInput" aria-describedby="inputHelp"
+                            onChange={
+                                (e) => {
+                                    setTarea(e.target.value);
+                                }
+                            }
+                            placeholder="No hay tareas, añadir tareas"/>
+                        <button type="submit" className="btn btn-primary btn-sm"
+                            onChange={
+                                (e) => {
+                                    setLista(e.target.value);
+                                }
+                            }
+                            onClick={enviarDatos}>
+                            Agregar tarea
+                        </button>
+                        
+                    </div>
+                    <div className="to-do-list d-flex">
+			  <ul>{lista.map((item, index) => (
+        <li key={index}>
+          {item}
+          <button className="btn" onClick={() => DeleteItems(index)}>
+            <i className="fas fa-trash-alt" />
+          </button>
+        </li>
+      ))}</ul>
+			</div>
+                </div>
+
+            </div>
+        </>
+    );
+};
 export default Home;
